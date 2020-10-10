43cf3724144bed55733b173e4f51f6e7354170aa
William <williamchenjian@gmail.com>
2020-10-10 13:58:59 -0400

    test3

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
   const [ connectCode, setConnectCode ] = useState('')
   const [ connected, setConnected] = useState(false)
   const [ name, setName ] = useState('')
-  const [ socket, setSocket ] = useState(io('http://67.205.150.186:80'))
+  const [ socket, setSocket ] = useState(io('https://67.205.150.186:80'))
   const [ players, setPlayers ] = useState([])
 
 
   function connect(){
-    const nsocket = io('http://67.205.150.186:80')
+    const nsocket = io('https://67.205.150.186:80')
 
     nsocket.on('connect', () => {
       nsocket.emit('join', connectCode)
